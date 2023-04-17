ae05e59f13d1cf7c39d500ec1a55316c5ff3f68d
Jean Marie Gustave MBONYINSHUTI <giggygustave.gg@gmail.com>
2023-04-17 02:38:07 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleDropdownClick = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleRegisterClick = () => {
+    setName('');
+    setPhoneNumber('');
+  };
+
   return (
     <div className="relative bg-white w-full h-[1139px] overflow-hidden text-right text-sm text-white font-montserrat">
       <div className="absolute top-[20px] left-[852px] w-[480px] h-[30px]" />
@@ -53,17 +75,29 @@ const App = () => {
               <div className="absolute top-[21.24px] left-[0px] bg-white w-[219.45px] h-[24.78px]" />
               <div className="absolute top-[113.4px] left-[0px] bg-lightsteelblue w-[219.45px] h-[1.42px]" />
               <div className="absolute top-[88.49px] left-[0px] bg-white w-[219.45px] h-[24.78px]" />
-              <div className="absolute top-[0px] left-[0px] leading-[13.87px] inline-block w-[53.8px]">
+              <label htmlFor="name"  className="absolute top-[0px] left-[0px] leading-[13.87px] inline-block w-[53.8px]">
                 Name*
+              </label>
+              <input
+                 type="text"
+                 id="name"
+                 name="name"
+                 value={name}
+                 onChange={handleNameChange}
+                 className="absolute top-[20.45px] left-[53.8px] w-[165.65px] h-[25.3px] bg-transparent border-[1px] border-solid border-midnightblue rounded-[3px] px-[10px] py-[5px] outline-none"
+             />
+              <label htmlFor="phoneNumber" className="absolute top-[67.25px] left-0 leading-[13.87px] inline-block w-[83.53px]">
+               Phone Number*
+              </label>
+              <input
+                 type="text"
+                 id="phoneNumber"
+                 name="phoneNumber"
+                 value={phoneNumber}
+                 onChange={handlePhoneNumberChange}
+                 className="absolute top-[84.17px] left-[83.53px] w-[135.92px] h-[25.3px] bg-transparent border-[1px] border-solid border-midnightblue rounded-[3px] px-[10px] py-[5px] outline-none"
+               />
               </div>
-              <div className="absolute top-[67.25px] left-[0px] leading-[13.87px] inline-block w-[83.53px]">
-                Phone Number*
-              </div>
-            </div>
-            <div className="absolute top-[252.72px] left-[126.71px] w-[15.9px] h-[8.82px]">
-              <div className="absolute top-[0px] left-[1px] bg-lightsteelblue w-[11.06px] h-[1.42px] [transform:_rotate(45deg)] [transform-origin:0_0]" />
-              <div className="absolute top-[7.82px] left-[7.08px] bg-lightsteelblue w-[11.06px] h-[1.42px] [transform:_rotate(-45deg)] [transform-origin:0_0]" />
-            </div>
             <div className="absolute top-[298.02px] left-[24.78px] rounded-[35.39px] bg-midnightblue box-border w-[219.45px] h-[29.73px] border-[0.7px] border-solid border-midnightblue" />
             <b className="absolute top-[23.36px] left-[15.57px] tracking-[1.42px] leading-[11.89px] uppercase inline-block w-[230.07px]">
               FORM TEMPLATE
@@ -73,29 +107,38 @@ const App = () => {
               consectetur, adipisci velit.
             </div>
           </div>
-          <div className="absolute top-[299.44px] left-[89.9px] text-[8.49px] tracking-[1.42px] leading-[28.32px] uppercase text-white inline-block w-[89.19px]">
-            register
+          <div className="absolute top-[299.44px] left-[89.9px] text-[8.49px] tracking-[1.42px] leading-[28.32px] uppercase text-white inline-block w-[89.19px] cursor-pointer" onClick={handleRegisterClick}>
+            Register
           </div>
         </div>
-        <div className="absolute top-[42px] left-[299.85px] w-[48.15px] h-[38.52px] text-[5.57px]">
-          <div className="absolute top-[15.2px] left-[0px] rounded-[3.48px] bg-white w-[48.15px] h-[23.31px]" />
-          <div className="absolute top-[0px] left-[5.57px] w-[41.05px] h-[34.97px]">
-            <img
-              className="absolute top-[0px] left-[31.93px] w-[9.12px] h-[9.12px] object-cover"
-              alt=""
-              src="/world-1-1@2x.png"
-            />
-            <div className="absolute top-[2px] left-[2.57px] text-[6.59px] leading-[6.89px] capitalize text-right inline-block w-5">
-              Login
+        <div className="relative">
+      <button
+        onClick={handleDropdownClick}
+        className="absolute top-[42px] left-[299.85px] w-[48.15px] h-[38.52px] text-[5.57px] focus:outline-none"
+      >
+        <div className="absolute top-[15.2px] left-[0px] rounded-[3.48px] bg-white w-[48.15px] h-[23.31px]" />
+        <div className="absolute top-[0px] left-[5.57px] w-[41.05px] h-[34.97px]">
+          <img
+            className="absolute top-[0px] left-[31.93px] w-[9.12px] h-[9.12px] object-cover"
+            alt=""
+            src="/world-1-1@2x.png"
+          />
+          <button className="absolute top-[2px] left-[2.57px] text-[6.59px] leading-[6.89px] capitalize text-right inline-block w-5 focus:outline-none">
+            Login
+          </button>
+          {isDropdownOpen && (
+            <div className="absolute top-[11px] left-[-5.5px] w-[53px] h-[29px] bg-white rounded-[3.48px] shadow-md">
+              <button className="w-full h-[14.5px] text-[6.59px] leading-[6.89px] text-midnightblue focus:outline-none">
+                English
+              </button>
+              <button className="w-full h-[14.5px] text-[6.59px] leading-[6.89px] text-lightsteelblue focus:outline-none">
+                Kinyarwanda
+              </button>
             </div>
-            <div className="absolute top-[16.72px] left-[0px] leading-[9.93px] text-midnightblue inline-block w-[38.52px] h-[9.12px]">
-              English
-            </div>
-            <div className="absolute top-[25.85px] left-[0px] leading-[9.93px] text-lightsteelblue inline-block w-[38.52px] h-[9.12px]">
-              Kinyarwanda
-            </div>
-          </div>
+          )}
         </div>
+      </button>
+    </div>
         <div className="absolute top-[959px] left-[95.9px] w-[216.62px] h-[76.09px] text-right">
           <img
             className="absolute top-[59.1px] left-[44.6px] w-[16.99px] h-[16.99px] object-cover"
@@ -176,3 +219,4 @@ const App = () => {
 };
 
 export default App;
+
